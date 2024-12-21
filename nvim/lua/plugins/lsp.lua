d53d03c82ef65d0a3e5a78d4aea4a2ddb8dc4c18
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-21 12:40:39 +0300

    Add formatting and event
    Enable autocompletion
    Format on save

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,23 +1,65 @@
 return {
 	'neovim/nvim-lspconfig',
+	event = 'VeryLazy',
+	dependencies = {
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
 	config = function()
-		local lsp = require'lspconfig'
+		local lsp = require 'lspconfig'
+
+		-- Pretty icons for warning messages
 		local icons = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
 		vim.diagnostic.config({
-  virtual_text = true,
-  signs = {
-		text = {
-		[vim.diagnostic.severity.ERROR] = icons.Error,
-    [vim.diagnostic.severity.WARN] = icons.Warn,
-    [vim.diagnostic.severity.HINT] = icons.Hint,
-    [vim.diagnostic.severity.INFO] = icons.Info,
+			virtual_text = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = icons.Error,
+					[vim.diagnostic.severity.WARN] = icons.Warn,
+					[vim.diagnostic.severity.HINT] = icons.Hint,
+					[vim.diagnostic.severity.INFO] = icons.Info,
+				}
+			},
+			underline = true,
+			update_in_insert = false,
+			severity_sort = false,
+		})
+
+		-- LSP Attach config
+		vim.api.nvim_create_autocmd('LspAttach', {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if  not client then return end
+				if client:supports_method('textDocument/implementation') then
+					-- Create a keymap for vim.lsp.buf.implementation
+				end
+
+				if client:supports_method('textDocument/completion') then
+					-- Enable auto-completion
+					vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+				end
+
+				if client:supports_method('textDocument/formatting') then
+					-- Format the current buffer on save
+					vim.api.nvim_create_autocmd('BufWritePre', {
+						buffer = args.buf,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+						end,
+					})
+				end
+			end,
+		})
+
+		-- LSP settings
+		lsp.lua_ls.setup {
 		}
-	},
-  underline = true,
-  update_in_insert = false,
-  severity_sort = false,
-})
-	lsp.lua_ls.setup{
-	}
 	end,
 }
