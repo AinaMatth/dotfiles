f8b23e61aaa28dc76b5f3a5f008bf5a4ad4bf4e7
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-21 12:19:42 +0300

    Add lua_ls, icons
    Add lua_ls for LSP
    Add icons for pretty

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,23 @@
 return {
 	'neovim/nvim-lspconfig',
+	config = function()
+		local lsp = require'lspconfig'
+		local icons = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+		vim.diagnostic.config({
+  virtual_text = true,
+  signs = {
+		text = {
+		[vim.diagnostic.severity.ERROR] = icons.Error,
+    [vim.diagnostic.severity.WARN] = icons.Warn,
+    [vim.diagnostic.severity.HINT] = icons.Hint,
+    [vim.diagnostic.severity.INFO] = icons.Info,
+		}
+	},
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+	lsp.lua_ls.setup{
+	}
+	end,
 }
