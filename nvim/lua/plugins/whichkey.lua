cec274ad63275986b27ee660b414e2a6fa9ae049
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-21 17:25:34 +0300

    group keys

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -7,6 +7,11 @@ return {
 			mappings = vim.g.have_nerd_font,
 			keys = {},
 		},
+		spec = {
+			{ '<leader>f', group = 'Find', mode = { 'n', 'x' } },
+			{ '<leader>b', group = 'Scratch Buffer', desc = "Scratch Buffer", icon = { icon = "", color = "cyan" } },
+			{ '<leader>G', group = 'Git', desc = "Git", icon = { icon = "󰊢", color = 'orange' }, mode = { 'n', 'v' } },
+			{ '<leader>z', group = 'Zen', desc = "Zen Mode", icon = { icon = "󱅻", color = "purple" }, mode = { 'n', 'v' } },
+		},
 	},
-	specs = {},
 }
