2ff2694df2a58b429c08ccb1343b51112d20ecd0
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-21 20:22:15 +0300

    update some groups

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,6 +1,9 @@
 return {
 	'folke/which-key.nvim',
 	event = 'VimEnter',
+	dependencies = {
+		'nvim-tree/nvim-web-devicons',
+	},
 	opts = {
 		preset = 'helix',
 		icons = {
@@ -8,9 +11,9 @@ return {
 			keys = {},
 		},
 		spec = {
-			{ '<leader>f', group = 'Find', mode = { 'n', 'x' } },
+			{ '<leader>f', group = 'Telescope', mode = { 'n', 'x' } },
 			{ '<leader>b', group = 'Scratch Buffer', desc = "Scratch Buffer", icon = { icon = "", color = "cyan" } },
-			{ '<leader>G', group = 'Git', desc = "Git", icon = { icon = "󰊢", color = 'orange' }, mode = { 'n', 'v' } },
+			{ '<leader>g', group = 'Git',mode = { 'n', 'v' } },
 			{ '<leader>z', group = 'Zen', desc = "Zen Mode", icon = { icon = "󱅻", color = "purple" }, mode = { 'n', 'v' } },
 		},
 	},
