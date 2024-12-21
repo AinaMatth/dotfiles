9eb11d464c5f3337440c3f2e6db7b07d32462d42
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-21 17:25:25 +0300

    minimalist config

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,10 +2,20 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	---@type snacks.Config
 	opts = {
 		bigfile = { enabled = true },
-		dashboard = { enabled = true },
+		dashboard = {
+			preset = {
+				header = [[
+ █████╗ ██╗███╗   ██╗ █████╗
+	██╔══██╗██║████╗  ██║██╔══██╗
+	███████║██║██╔██╗ ██║███████║
+	██╔══██║██║██║╚██╗██║██╔══██║
+	██║  ██║██║██║ ╚████║██║  ██║
+	╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
+]],
+			}
+		},
 		indent = { enabled = true },
 		input = { enabled = true },
 		notifier = {
@@ -15,55 +25,15 @@ return {
 		quickfile = { enabled = true },
 		scroll = { enabled = true },
 		scope = { enabled = true },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
-		styles = {
-			notification = {
-				wo = { wrap = true } -- Wrap notifications
-			}
-		}
 	},
 	keys = {
-		{ "<leader>zn", function() Snacks.zen() end,                desc = "Toggle Zen Mode" },
-		{ "<leader>zz", function() Snacks.zen.zoom() end,           desc = "Toggle Zoom" },
-		{ "<leader>s.", function() Snacks.scratch() end,            desc = "Toggle Scratch Buffer" },
-		{ "<leader>ss", function() Snacks.scratch.select() end,     desc = "Select Scratch Buffer" },
-		{ "<leader>bd", function() Snacks.bufdelete() end,          desc = "Delete Buffer" },
-		{ "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
-		{ "<leader>gB", function() Snacks.gitbrowse() end,          desc = "Git Browse",                  mode = { "n", "v" } },
-		{ "<leader>gb", function() Snacks.git.blame_line() end,     desc = "Git Blame Line" },
-		{ "<leader>gf", function() Snacks.lazygit.log_file() end,   desc = "Lazygit Current File History" },
-		{ "<leader>gg", function() Snacks.lazygit() end,            desc = "Lazygit" },
-		{ "<leader>gl", function() Snacks.lazygit.log() end,        desc = "Lazygit Log (cwd)" },
-		{ "<leader>un", function() Snacks.notifier.hide() end,      desc = "Dismiss All Notifications" },
+		{ "<leader>zn", function() Snacks.zen() end,              desc = "Zen Mode" },
+		{ "<leader>zz", function() Snacks.zen.zoom() end,         desc = "Zen Zoom" },
+		{ "<leader>bs", function() Snacks.scratch() end,          desc = "Scratch Buffer" },
+		{ "<leader>bS", function() Snacks.scratch.select() end,   desc = "Scratch Buffer Selection" },
+		{ "<leader>Gb", function() Snacks.git.blame_line() end,   desc = "Git Blame Line" },
+		{ "<leader>Gf", function() Snacks.lazygit.log_file() end, desc = "git Current File History" },
+		{ "<leader>Gg", function() Snacks.lazygit() end,          desc = "git" },
+		{ "<leader>Gl", function() Snacks.lazygit.log() end,      desc = "git Log (cwd)" },
 	},
-	init = function()
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "VeryLazy",
-			callback = function()
-				-- Setup some globals for debugging (lazy-loaded)
-				_G.dd = function(...)
-					Snacks.debug.inspect(...)
-				end
-				_G.bt = function()
-					Snacks.debug.backtrace()
-				end
-				vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-				-- Create some toggle mappings
-				Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-				Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-				Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-				Snacks.toggle.diagnostics():map("<leader>ud")
-				Snacks.toggle.line_number():map("<leader>ul")
-				Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
-					"<leader>uc")
-				Snacks.toggle.treesitter():map("<leader>uT")
-				Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-				Snacks.toggle.inlay_hints():map("<leader>uh")
-				Snacks.toggle.indent():map("<leader>ug")
-				Snacks.toggle.dim():map("<leader>uD")
-			end,
-		})
-	end,
 }
