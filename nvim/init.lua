e4485a4100afb653d731bcc287bcbd0a3350c0a4
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-22 08:15:43 +0300

    Highlight yank autocommand

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,13 @@
 require('core.options')
-require('core.autocommands')
 require('core.keymaps')
+
+-- Highlight text on Yank
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.hl.on_yank {higroup= highlight_group,pattern = '*',timeout=300}
+  end,
+})
+
 require('core.lazy')
