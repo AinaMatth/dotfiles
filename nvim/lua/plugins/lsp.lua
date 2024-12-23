a9530c4049fb22863375f3d13aa53d5aca394b60
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-23 16:07:08 +0300

    Modular config for LSP

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,11 +1,11 @@
 return {
 	{
 		'neovim/nvim-lspconfig',
+		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			{ 'williamboman/mason.nvim', config = true }, -- Must be loaded before dependants
+			{ 'williamboman/mason.nvim', config = true },
 			'williamboman/mason-lspconfig.nvim',
 			'WhoIsSethDaniel/mason-tool-installer.nvim',
-			{ 'j-hui/fidget.nvim',       opts = {} },
 			{
 				'folke/lazydev.nvim',
 				ft = 'lua',
@@ -15,92 +15,32 @@ return {
 					},
 				},
 			},
-			{ 'saghen/blink.cmp' },
-		},
-		config = function()
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('LspAttachGroup', { clear = true }), -- Group for LSP attach events
-				callback = function(args)
-					local client = vim.lsp.get_client_by_id(args.data.client_id)
-					if not client then return end
-
-					-- Key mappings for LSP features
-					local function map_key(keys, func, desc)
-						vim.keymap.set('n', keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
-					end
-
-					if client:supports_method('textDocument/implementation') then
-						map_key('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-					end
-
-					if client:supports_method('textDocument/formatting') then
-						-- Format the current buffer on save
-						vim.api.nvim_create_autocmd('BufWritePre', {
-							buffer = args.buf,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-							end,
-						})
-					end
-				end,
-			})
-			-- Diagnostic configuration
-			local icons = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
-			vim.diagnostic.config({
-				virtual_text = true,
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = icons.Error,
-						[vim.diagnostic.severity.WARN]  = icons.Warn,
-						[vim.diagnostic.severity.HINT]  = icons.Hint,
-						[vim.diagnostic.severity.INFO]  = icons.Info,
-					}
-				},
-				underline = true,
-				update_in_insert = false,
-				severity_sort = false,
-			})
-
-			-- Setup capabilities for LSP clients
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities())
-
-			-- Define LSP servers configuration
-			local servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = 'Replace',
-							},
-							diagnostics = { disable = { 'missing-fields' } },
+			{
+				'j-hui/fidget.nvim',
+				tag = 'v1.4.0',
+				opts = {
+					progress = {
+						display = {
+							done_icon = '✓',
+						},
+					},
+					notification = {
+						window = {
+							winblend = 0,
 						},
 					},
 				},
-			}
-
-			-- Mason setup
-			require('mason').setup()
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, { 'stylua' })
-			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-			require('mason-lspconfig').setup {
-				handlers = {
-					function(server_name)
-						local server_configurations = servers[server_name] or {}
-						server_configurations.capabilities =
-								vim.tbl_deep_extend('force', {}, capabilities, server_configurations.capabilities or {})
-						require('lspconfig')[server_name].setup(server_configurations)
-					end,
-				},
-			}
+			},
+			{ 'saghen/blink.cmp' },
+		},
+		config = function()
+			require('core.lsp').setup() -- Call the setup function from the module
 		end,
 	},
 	{
-		"pmizio/typescript-tools.nvim",
+		'pmizio/typescript-tools.nvim',
+		ft = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "html" }, -- Load only for relevant file types
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {},
 	},
-
 }
