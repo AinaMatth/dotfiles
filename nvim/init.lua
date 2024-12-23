b5130c0e060d405a4f7751fc36d4563874fed366
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-23 16:08:35 +0300

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,10 @@
+-- [[Leader key]]
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 require('core.options')
 require('core.keymaps')
-
--- Highlight text on Yank
-
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.hl.on_yank {higroup= highlight_group,pattern = '*',timeout=300}
-  end,
-})
-
+require('core.autocmds')
 require('core.lazy')
