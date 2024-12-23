3105ab2bedbdd5662b2012bf620407e28a412d26
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-23 18:29:32 +0300

    typescript

---
+++ nvim/lua/plugins/lsp/ts.lua
@@ -0,0 +1,6 @@
+return {
+	"pmizio/typescript-tools.nvim",
+	ft = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "html" }, -- Load only for relevant file types
+	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	opts = {},
+}
