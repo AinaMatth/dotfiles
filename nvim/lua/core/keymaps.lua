88cfb6e5edf5676cbb1aab76f5ad9ba2bd827622
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-23 17:40:53 +0300

    Format file

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -4,10 +4,10 @@
 local key = vim.keymap.set
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-key('n', '<Esc>', '<cmd>nohlsearch<CR>')
+key("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Exit terminal mode
-key('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+key("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- save file
 key({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
@@ -16,4 +16,7 @@ key({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 key("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
 -- Lazy
-key('n', '<leader>l', '<cmd>:Lazy<CR>', { desc = 'Lazy' })
+key("n", "<leader>l", "<cmd>:Lazy<CR>", { desc = "Lazy" })
+
+-- Lazy
+key("n", "<leader>m", "<cmd>:Mason<CR>", { desc = "Mason" })
