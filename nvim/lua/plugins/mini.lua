dc9f212b6d60898a72e6dce8cdbdc8470c074199
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-23 16:07:26 +0300

    Update options

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,28 +1,29 @@
 return {
 	{
 		'echasnovski/mini.pairs',
-		version = '*',
-		event = 'VeryLazy',
+		ft = { "lua", "javascript", "typescript", "bash", "python", "html", "css", "php", "rust", "java" }, -- Specify programming file types
 		config = function()
 			require('mini.pairs').setup({
-				modes = { insert = true, command = true, terminal = false },
-				-- skip autopair when next character is one of these
-				skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-				-- skip autopair when the cursor is inside these treesitter nodes
-				skip_ts = { "string" },
-				-- skip autopair when next character is closing pair
-				-- and there are more closing pairs than opening pairs
-				skip_unbalanced = true,
-				-- better deal with markdown code blocks
-				markdown = true,
+				modes = { insert = true, command = true }, -- Enable in insert and command modes
+				skip_next = [=[[%w%%%'%[%"%.%`%$]]=],  -- Characters to skip
+				skip_ts = { "string" },                -- Skip pairs in strings
+				skip_unbalanced = true,                -- Skip unbalanced pairs
+				markdown = true,                       -- Enable for markdown files (optional)
+				-- Custom pairs for HTML tags
+				custom_pairs = {
+					['<'] = '>', -- Auto-close HTML tags
+					['{'] = '}', -- Auto-close curly braces
+					['('] = ')', -- Auto-close parentheses
+					['['] = ']', -- Auto-close brackets
+				},
 			})
 		end,
-
 	},
 	{
 		'echasnovski/mini.surround',
 		version = '*',
 		recommended = true,
+		event = 'VeryLazy',
 		config = function()
 			require('mini.surround').setup()
 		end,
