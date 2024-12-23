12eeb85dbe86707186851a7a1c78f735ea7442d1
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-23 20:59:42 +0300

    fix lualine on dashboard

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,23 +1,23 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	event = 'VeryLazy',
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
 	config = function()
-		local env_var_nvim_theme = os.getenv 'NVIM_THEME' or 'tokyonight'
+		local env_var_nvim_theme = os.getenv("NVIM_THEME") or "tokyonight"
 
 		local themes = {
-			tokyonight = 'tokyonight-storm',
+			tokyonight = "tokyonight-storm",
 		}
 		local mode = {
-			'mode',
+			"mode",
 			fmt = function(str)
 				-- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
-				return ' ' .. str
+				return " " .. str
 			end,
 		}
 
 		local filename = {
-			'filename',
+			"filename",
 			file_status = true, -- displays file status (readonly status, modified status)
 			path = 0,        -- 0 = just filename, 1 = relative path, 2 = absolute path
 		}
@@ -27,10 +27,10 @@ return {
 		end
 
 		local diagnostics = {
-			'diagnostics',
-			sources = { 'nvim_diagnostic' },
-			sections = { 'error', 'warn' },
-			symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " ", info = " ", hint = " " },
 			colored = false,
 			update_in_insert = false,
 			always_visible = false,
@@ -38,25 +38,31 @@ return {
 		}
 
 		local diff = {
-			'diff',
+			"diff",
 			colored = true,
-			symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+			symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
 			cond = hide_in_width,
 		}
-		require('lualine').setup({
+		require("lualine").setup({
 			options = {
 				theme = themes[env_var_nvim_theme],
-				section_separators = { left = '', right = '' },
-				component_separators = { left = '', right = '' },
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+				disabled_filetypes = { "snacks_dashboard" },
 				always_divide_middle = true,
 			},
 			sections = {
 				lualine_a = { mode },
-				lualine_b = { 'branch' },
+				lualine_b = { "branch" },
 				lualine_c = { filename },
-				lualine_x = { diagnostics, diff, { 'encoding', cond = hide_in_width }, { 'filetype', cond = hide_in_width } },
-				lualine_y = { 'location' },
-				lualine_z = { 'progress' },
+				lualine_x = {
+					diagnostics,
+					diff,
+					{ "encoding", cond = hide_in_width },
+					{ "filetype", cond = hide_in_width },
+				},
+				lualine_y = { "location" },
+				lualine_z = { "progress" },
 			},
 		})
 	end,
