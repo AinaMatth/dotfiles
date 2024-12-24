cc870bded90eee32d101495a57963252481d55a9
AinaMatth <10987778+AinaMatth@users.noreply.github.com>
2024-12-24 08:07:55 +0300

    update

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,18 +1,17 @@
 return {
-	'saghen/blink.cmp',
-	event = 'InsertEnter',
-	dependencies = 'rafamadriz/friendly-snippets',
-	version = '*',
+	"saghen/blink.cmp",
+	event = "InsertEnter",
+	dependencies = "rafamadriz/friendly-snippets",
+	version = "*",
 	opts = {
-		keymap = { preset = 'default' },
-
+		keymap = { preset = "default" },
 		appearance = {
 			use_nvim_cmp_as_default = true,
-			nerd_font_variant = 'mono'
+			nerd_font_variant = "mono",
 		},
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer' },
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 	},
-	opts_extend = { "sources.default" }
+	opts_extend = { "sources.default" },
 }
