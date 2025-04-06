a3c653f05646ee2775079a4940e15546ae136ecb
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-06 23:10:47 +0300

    feat: new formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,41 @@
+return {
+  -- Autoformat
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        liquid = { "prettier" },
+        lua = { "stylua" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        python = { "isort", "black" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        async = false,
+        lsp_format = "fallback",
+      },
+    },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end,
+        mode = "",
+        desc = "[F]ormat buffer",
+      },
+    },
+  },
+}
