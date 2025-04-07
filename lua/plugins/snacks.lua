4caa814de180a1088401ec635e6e8fe7cca11fa5
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-07 09:20:06 +0300

    feat: add colorscript

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,15 +8,15 @@ return {
     dashboard = {
       sections = {
         { section = "header" },
-        { section = "keys",  gap = 1, padding = 1 },
         {
           pane = 2,
-          icon = " ",
-          title = "Recent Files",
-          section = "recent_files",
-          indent = 2,
+          section = "terminal",
+          cmd = "colorscript -e alpha",
+          height = 5,
           padding = 1,
         },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         {
           pane = 2,
