4e5dce804ea8411c3ff94c313d3dcbefdb3d03ae
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-09 21:07:15 +0300

    style: format

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,108 +1,108 @@
 -- Function to create an autogroup if it doesn't exist
 local function augroup(name)
-  return vim.api.nvim_create_augroup(name, { clear = true })
+	return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = augroup 'highlight_yank',
-  callback = function()
-    (vim.hl or vim.highlight).on_yank()
-  end,
+	group = augroup 'highlight_yank',
+	callback = function()
+		(vim.hl or vim.highlight).on_yank()
+	end,
 })
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup 'close_with_q',
-  pattern = {
-    'PlenaryTestPopup',
-    'checkhealth',
-    'dbout',
-    'help',
-    'lspinfo',
-    'qf',
-    'spectre_panel',
-    'startuptime',
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set('n', 'q', function()
-      vim.cmd 'close'
-      pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
-    end, {
-      buffer = event.buf,
-      silent = true,
-      desc = 'Quit buffer',
-    })
-  end,
+	group = augroup 'close_with_q',
+	pattern = {
+		'PlenaryTestPopup',
+		'checkhealth',
+		'dbout',
+		'help',
+		'lspinfo',
+		'qf',
+		'spectre_panel',
+		'startuptime',
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set('n', 'q', function()
+			vim.cmd 'close'
+			pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+		end, {
+			buffer = event.buf,
+			silent = true,
+			desc = 'Quit buffer',
+		})
+	end,
 })
 
 -- Format on Save
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    -- Auto-format ("lint") on save.
-    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-    if
-        not client:supports_method 'textDocument/willSaveWaitUntil'
-        and client:supports_method 'textDocument/formatting'
-    then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format { bufnr = args.buf, id = client.id, timeout_ms = 1000 }
-        end,
-      })
-    end
-  end,
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		-- Auto-format ("lint") on save.
+		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+		if
+				not client:supports_method 'textDocument/willSaveWaitUntil'
+				and client:supports_method 'textDocument/formatting'
+		then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format { bufnr = args.buf, id = client.id, timeout_ms = 1000 }
+				end,
+			})
+		end
+	end,
 })
 
 -- Advanced LSP progress
 ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
 local progress = vim.defaulttable()
 vim.api.nvim_create_autocmd('LspProgress', {
-  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    local value = ev.data.params
-    .value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
-    if not client or type(value) ~= 'table' then
-      return
-    end
-    local p = progress[client.id]
+	---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		local value = ev.data.params
+		.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+		if not client or type(value) ~= 'table' then
+			return
+		end
+		local p = progress[client.id]
 
-    for i = 1, #p + 1 do
-      if i == #p + 1 or p[i].token == ev.data.params.token then
-        p[i] = {
-          token = ev.data.params.token,
-          msg = ('[%3d%%] %s%s'):format(
-            value.kind == 'end' and 100 or value.percentage or 100,
-            value.title or '',
-            value.message and (' **%s**'):format(value.message) or ''
-          ),
-          done = value.kind == 'end',
-        }
-        break
-      end
-    end
+		for i = 1, #p + 1 do
+			if i == #p + 1 or p[i].token == ev.data.params.token then
+				p[i] = {
+					token = ev.data.params.token,
+					msg = ('[%3d%%] %s%s'):format(
+						value.kind == 'end' and 100 or value.percentage or 100,
+						value.title or '',
+						value.message and (' **%s**'):format(value.message) or ''
+					),
+					done = value.kind == 'end',
+				}
+				break
+			end
+		end
 
-    local msg = {} ---@type string[]
-    progress[client.id] = vim.tbl_filter(function(v)
-      return table.insert(msg, v.msg) or not v.done
-    end, p)
+		local msg = {} ---@type string[]
+		progress[client.id] = vim.tbl_filter(function(v)
+			return table.insert(msg, v.msg) or not v.done
+		end, p)
 
-    local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
-    vim.notify(table.concat(msg, '\n'), 'info', {
-      id = 'lsp_progress',
-      title = client.name,
-      opts = function(notif)
-        notif.icon = #progress[client.id] == 0 and ' '
-            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-      end,
-    })
-  end,
+		local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+		vim.notify(table.concat(msg, '\n'), 'info', {
+			id = 'lsp_progress',
+			title = client.name,
+			opts = function(notif)
+				notif.icon = #progress[client.id] == 0 and ' '
+						or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+			end,
+		})
+	end,
 })
 
-vim.keymap.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Lazy' })
+vim.keymap.set('n', '<leader>ll', '<cmd>Lazy<CR>', { desc = 'Lazy' })
