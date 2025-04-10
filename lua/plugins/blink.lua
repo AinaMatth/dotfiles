0fdde99c9c70a1ceb78c1f224a49d69a91cf88e8
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-10 14:59:19 +0300

    revert: previous config

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,24 @@
+-- -- Autocompletion
+return {
+	'saghen/blink.cmp',
+	dependencies = { 'rafamadriz/friendly-snippets' },
+	event = 'InsertEnter',
+	version = '1.*',
+	build = 'cargo build --release',
+
+	---@module 'blink.cmp'
+	opts = {
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = 'default' },
+
+		appearance = {
+			nerd_font_variant = 'mono',
+		},
+		completion = { documentation = { auto_show = true } },
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
+		fuzzy = { implementation = 'prefer_rust_with_warning' },
+	},
+	opts_extend = { 'sources.default' },
+}
