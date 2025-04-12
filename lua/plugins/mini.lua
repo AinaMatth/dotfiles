0a0c4ecc913cafd7bc705ba8cde674586a195140
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-12 22:29:41 +0300

    feat: rework the file for modularity

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,41 +2,11 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
+    local statusline = require 'plugins.mini.statusline'
+    require('mini.statusline').setup(statusline.config)
+    local clue = require 'plugins.mini.clue'
+    require('mini.clue').setup(clue.config)
     require('mini.icons').setup {}
-    require('mini.statusline').setup {
-      content = {
-        active = function()
-          local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
-          local git = MiniStatusline.section_git { trunc_width = 40 }
-          local diff = MiniStatusline.section_diff { trunc_width = 75 }
-          local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
-          local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
-          local filename = MiniStatusline.section_filename { trunc_width = 140 }
-          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
-          local position = string.format('Line %d:%d', vim.fn.line '.', vim.fn.col '.')
-          return MiniStatusline.combine_groups {
-            { hl = mode_hl,                  strings = { mode } },
-            { hl = 'MiniStatuslineDevinfo',  strings = { git, diff } },
-            { hl = 'MiniStatuslineFilename', strings = { filename } },
-            '%<', -- Mark truncate point
-            '%=', -- End left alignment
-            { hl = 'MiniStatuslineDevinfo',  strings = { diagnostics, lsp } },
-            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-            { hl = mode_hl,                  strings = { position } },
-          }
-        end,
-
-        -- Keep inactive windows simple as in NvChad
-        inactive = function()
-          local filename = MiniStatusline.section_filename { trunc_width = 140 }
-          return MiniStatusline.combine_groups {
-            { hl = 'MiniStatuslineInactive', strings = { filename } },
-          }
-        end,
-      },
-      use_icons = true,
-      set_vim_settings = true,
-    }
     vim.api.nvim_create_user_command('MiniFiles', function()
       require('mini.files').setup {
         windows = {
@@ -56,7 +26,17 @@ return {
     -- Delay load non-essential modules
     vim.defer_fn(function()
       require('mini.indentscope').setup {}
-      require('mini.diff').setup { view = { style = 'sign' } }
+      require('mini.notify').setup {}
+      require('mini.diff').setup {
+        view = {
+          style = 'sign',
+          signs = {
+            add = '▎',
+            change = '▎',
+            delete = '',
+          },
+        },
+      }
       require('mini.git').setup {}
     end, 100) -- 100ms delay after startup
 
