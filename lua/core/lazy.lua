816ed9cecebb501b27e3c4c5a55f5f0a86700258
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-15 16:42:46 +0300

    perf: disable some plugins

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -16,11 +16,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require("lazy").setup({
+require('lazy').setup {
   spec = {
-    { import = "plugins" },
+    { import = 'plugins' },
   },
-    defaults = {
+  defaults = {
     lazy = false,
     version = false,
   },
@@ -32,42 +32,57 @@ require("lazy").setup({
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+        '2html_plugin',
+        'tohtml',
+        'getscript',
+        'getscriptPlugin',
+        'gzip',
+        'logipat',
+        'matchit',
+        'tar',
+        'tarPlugin',
+        'rrhelper',
+        'spellfile_plugin',
+        'vimball',
+        'vimballPlugin',
+        'zip',
+        'zipPlugin',
+        'tutor',
+        'rplugin',
+        'syntax',
+        'synmenu',
+        'optwin',
+        'compiler',
+        'bugreport',
+        'ftplugin',
       },
     },
   },
   ui = {
-		icons = {
-			cmd = "  ", ---+ ${ui, Lazy.nvim UI icons}
+    icons = {
+      cmd = '  ', ---+ ${ui, Lazy.nvim UI icons}
 
-			config = "  ",
-			event = "  ",
-			ft = "  ",
+      config = '  ',
+      event = '  ',
+      ft = '  ',
 
-			init = "  ",
-			imports = "  ",
+      init = '  ',
+      imports = '  ',
 
-			keys = "  ",
+      keys = '  ',
 
-			lazy = " ",
-			loaded = " ",
-			not_loaded = " ",
+      lazy = ' ',
+      loaded = ' ',
+      not_loaded = ' ',
 
-			plugin = "  ",
-			runtime = "  ",
-			require = "  ",
+      plugin = '  ',
+      runtime = '  ',
+      require = '  ',
 
-			source = " ",
-			start = "",
+      source = ' ',
+      start = '',
 
-			task = "  " ---_
-		}
-	}
-})
+      task = '  ', ---_
+    },
+  },
+}
