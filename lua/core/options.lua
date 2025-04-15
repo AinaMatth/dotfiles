416dd198661d32b7ee26bf8a311efa7aa197c989
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-15 10:06:45 +0300

    refactor: redo old config

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,10 +2,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.g.have_nerd_font = true
-vim.o.background = "dark"
+vim.o.background = 'dark'
 vim.o.termguicolors = true
-vim.cmd.colorscheme("habamax")
-vim.o.completeopt = "menu,menuone,popup,fuzzy"
+vim.cmd.colorscheme 'habamax'
+vim.o.completeopt = 'menu,menuone,popup,fuzzy'
 
 vim.g.loaded_node_provider = 0
 vim.g.loaded_python3_provider = 0
@@ -13,12 +13,12 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 vim.schedule(function()
-  vim.opt.clipboard:append("unnamedplus")
+  vim.opt.clipboard:append 'unnamedplus'
 end)
 
 vim.opt.cursorline = true
 vim.opt.number = true
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = 'yes'
 vim.opt.timeoutlen = 300
 vim.opt.pumheight = 10
 vim.opt.ignorecase = true
@@ -27,12 +27,12 @@ vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 10
 vim.opt.list = true
 vim.opt.listchars = {
-  tab = "▶ ",
-  trail = "·",
-  extends = "»",
-  precedes = "«",
+  tab = '▶ ',
+  trail = '·',
+  extends = '»',
+  precedes = '«',
 }
-vim.opt.diffopt:append("linematch:60")
+vim.opt.diffopt:append 'linematch:60'
 vim.opt.confirm = true
 vim.opt.updatetime = 200
 vim.opt.smartindent = true
@@ -40,13 +40,14 @@ vim.opt.shiftround = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
+vim.o.statuscolumn = ''
 vim.opt.undofile = true
 vim.opt.undolevels = 10000
 vim.g.netrw_banner = 0
 vim.opt.fillchars = {
-  eob = " ",
-  foldopen = "▾",
-  foldclose = "▸",
+  eob = ' ',
+  foldopen = '▾',
+  foldclose = '▸',
 }
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 5
@@ -55,10 +56,6 @@ vim.opt.history = 200
 vim.diagnostic.config {
   virtual_lines = true,
   underline = false,
-  float = {
-    border = 'single',
-    severity_sort = false,
-  },
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = '',
---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,37 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
+  opts = {
+    preset = 'helix',
+    defaults = {},
+    spec = {
+      {
+        mode = { 'n', 'v' },
+        { '<leader>c', group = 'code' },
+        { '<leader>d', group = 'debug' },
+        { '<leader>dp', group = 'profiler' },
+        { '<leader>f', group = 'file/find' },
+        { '<leader>g', group = 'git', icon = { icon = '󰊢', color = 'red' } },
+        { '<leader>gh', group = 'hunks' },
+        { '<leader>s', group = 'search' },
+        { '<leader>u', group = 'ui', icon = { icon = '󰙵 ', color = 'cyan' } },
+        { '<leader>x', group = 'diagnostics/quickfix', icon = { icon = '󱖫 ', color = 'green' } },
+        { '[', group = 'prev' },
+        { ']', group = 'next' },
+        { 'g', group = 'goto' },
+        { 'gs', group = 'surround' },
+        { 'z', group = 'fold' },
+        { 'gx', desc = 'Open with system app' },
+      },
+    },
+  },
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Keymaps (which-key)',
+    },
+  },
+}
