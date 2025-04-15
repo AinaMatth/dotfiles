8abdb47ced31d9aa24c2b4d7bf3596dce05d759c
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-15 16:43:13 +0300

    perf: add event

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,19 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  event = { 'BufReadPost', 'BufNewFile' },
+  cmd = {
+    'EditQuery',
+    'Inspect',
+    'InspectTree',
+    'TSBufDisable',
+    'TSBufEnable',
+    'TSDisable',
+    'TSEnable',
+    'TSInstall',
+    'TSModuleInfo',
+    'TSUpdate',
+    'TSUpdateSync',
+  },
   build = ':TSUpdate',
   config = function()
     local configs = require 'nvim-treesitter.configs'
@@ -32,8 +46,23 @@ return {
         'yaml',
       },
       sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+      highlight = {
+        enable = true,
+        use_languagetree = true,
+        indent = { enable = true },
+        highlight = { enable = true },
+
+        additional_vim_regex_highlighting = false,
+
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
     }
   end,
 }
