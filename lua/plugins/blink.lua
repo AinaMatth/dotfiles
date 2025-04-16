5791fd36a3ce01c30075081c1e33f5fa4f4add3c
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-16 09:24:34 +0300

    feat: add LuaSnip

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,9 +1,31 @@
 return {
   'saghen/blink.cmp',
   event = { 'InsertEnter', 'CmdlineEnter' },
-  dependencies = { 'rafamadriz/friendly-snippets' },
   version = '1.*',
   build = 'cargo build --release',
+  dependencies = {
+    -- Snippet Engine
+    {
+      'L3MON4D3/LuaSnip',
+      version = '2.*',
+      build = (function()
+        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+          return
+        end
+        return 'make install_jsregexp'
+      end)(),
+      dependencies = {
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+          end,
+        },
+      },
+      opts = {},
+    },
+    'folke/lazydev.nvim',
+  },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -11,10 +33,14 @@ return {
     appearance = {
       nerd_font_variant = 'normal',
     },
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'snippets', 'lazydev' },
+      providers = {
+        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+      },
     },
+    signature = { enabled = true },
     fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
   opts_extend = { 'sources.default' },
