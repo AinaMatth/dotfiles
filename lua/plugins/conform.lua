85300e1716cc8d46e4bf42efa2242dbbdc9b8185
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-19 11:21:17 +0300

    feat: ruff new python formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,8 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      python = { 'isort', 'black', stop_after_first = true },
+      python = { 'ruff', stop_after_first = true },
+      python = { 'ruff', stop_after_first = true },
       -- rust = { "rustfmt", lsp_format = "fallback" },
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
