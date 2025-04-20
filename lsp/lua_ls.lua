c272cdcb225ff6ba6c6d1426788482dc5b390d71
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-20 08:39:17 +0300

    feat: add more settings

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -4,6 +4,12 @@ return {
   root_markers = { '.luarc.json', '.luarc.jsonc' },
   settings = {
     Lua = {
+      completion = { callSnippet = 'Replace' },
+      format = { enable = false },
+      hint = {
+        enable = true,
+        arrayIndex = 'Disable',
+      },
       runtime = {
         version = 'LuaJIT',
       },
