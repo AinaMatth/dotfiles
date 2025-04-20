5d6f89dd48d0337f30978c2ffef44d01986ddddc
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-20 08:39:11 +0300

    fix: root_markers

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,7 +1,15 @@
 return {
   cmd = { 'clangd' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
-  { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' },
+  root_markers = {
+    '.clangd',
+    '.clang-tidy',
+    '.clang-format',
+    'compile_commands.json',
+    'compile_flags.txt',
+    'configure.ac',
+    '.git',
+  },
   settings = {
     capabilities = {
       offsetEncoding = { 'utf-8', 'utf-16' },
