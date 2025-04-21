fa24df9c94a3019eeafe6bf5dc51508118708cc8
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-21 18:07:45 +0300

    feat: update event

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,6 +1,6 @@
 return {
   'folke/todo-comments.nvim',
-  event = 'VimEnter',
+  event = { 'BufReadPost', 'BufNewFile', 'VeryLazy' },
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
     keywords = {
