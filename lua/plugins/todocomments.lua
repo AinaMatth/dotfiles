a8c8b77b59baecbfdf2fbcb783a909a0aeb8ab3f
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-21 10:28:03 +0300

    feat: icons and pickers

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -2,5 +2,25 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  opts = {
+    keywords = {
+      TEST = { icon = '󱎫', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+    },
+  },
+  keys = {
+    {
+      '<leader>st',
+      function()
+        Snacks.picker.todo_comments { keywords = { 'TODO', 'HACK', 'WARNING', 'BUG', 'NOTE', 'INFO', 'PERF', 'ERROR' } }
+      end,
+      desc = 'Todo Comment Tags',
+    },
+    {
+      '<leader>sT',
+      function()
+        Snacks.picker.todo_comments()
+      end,
+      desc = 'Todo',
+    },
+  },
 }
