48cba174589170e0f438cb41f07702b94f4f0d46
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-21 18:37:58 +0300

    feat: add LSP progress autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -67,3 +67,29 @@ vim.api.nvim_create_autocmd('FileType', {
     end)
   end,
 })
+vim.api.nvim_create_autocmd('LspProgress', {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+    vim.notify(vim.lsp.status(), 'info', {
+      id = 'lsp_progress',
+      title = 'LSP Progress',
+      opts = function(notif)
+        notif.icon = ev.data.params.value.kind == 'end' and ' '
+          or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = vim.api.nvim_create_augroup('auto_create_dir', { clear = true }),
+  callback = function(event)
+    if event.match:match '^%w%w+:[\\/][\\/]' then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
