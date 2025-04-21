ded1634c801004cff3085a3c3d67f3c295ffd42d
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-21 18:07:32 +0300

    feat: opts for transparency

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,14 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  event = 'VeryLazy',
+  event = 'BufReadPre',
   opts = {
     options = {
       theme = 'tokyonight',
       component_separators = { left = '', right = '' },
       section_separators = { left = '', right = '' },
+      globalstatus = vim.o.laststatus == 3,
+      disabled_filetypes = { statusline = { 'dashboard', 'alpha', 'ministarter', 'snacks_dashboard' } },
     },
   },
 }
