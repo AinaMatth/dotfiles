7acc08e6ce6f2cf4ae822360e69dbac1da21003b
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-21 18:07:59 +0300

    style: transparent background settings

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -4,9 +4,17 @@ return {
   config = function()
     ---@diagnostic disable-next-line: missing-fields
     require('tokyonight').setup {
+      style = 'storm',
       transparent = true,
-      styles = { comments = { italic = false }, sidebars = 'transparent', floats = 'transparent' },
-      lualine_bold = true,
+      styles = {
+        comments = { italic = false },
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+      on_highlights = function(highlights, colors)
+        -- Colors for Snacks pickers
+        highlights.SnacksPickerBoxTitle = { bg = '#1c99f2', fg = '#ffffff', bold = true }
+      end,
     }
     vim.cmd.colorscheme 'tokyonight'
   end,
