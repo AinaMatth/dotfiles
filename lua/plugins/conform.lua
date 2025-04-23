f29903efa2a44990f6bb4c76073aaa9653b5b35f
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-23 22:10:26 +0300

    feat: add ruff

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,9 +6,9 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'ruff', stop_after_first = true },
-      python = { 'ruff', stop_after_first = true },
+      sql = { 'sqruff', stop_after_first = true },
       -- rust = { "rustfmt", lsp_format = "fallback" },
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { 'biome', stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,
