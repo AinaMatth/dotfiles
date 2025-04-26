50dd718221614dacba85d89776635c105150d41e
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-26 11:42:12 +0300

    feat: prettier and prettierd

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,17 +2,26 @@ return {
   'stevearc/conform.nvim',
   cmd = { 'ConformInfo', 'ConformFormat', 'ConformFormat', 'ConformToggleFormatGlobal' },
   event = 'BufWritePre',
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'ruff', stop_after_first = true },
       sql = { 'sqruff', stop_after_first = true },
       -- rust = { "rustfmt", lsp_format = "fallback" },
-      javascript = { 'biome', stop_after_first = true },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      c = { 'clang-format' },
+    },
+    default_format_opts = {
+      lsp_format = 'fallback',
     },
     format_on_save = {
       timeout_ms = 500,
       lsp_format = 'fallback',
     },
   },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
