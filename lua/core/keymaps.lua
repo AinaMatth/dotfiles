bf0fdefaddaa3272809f500951d9ff10762bbb15
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-27 11:23:43 +0300

    fix: keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,7 +5,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- Resize splits using Ctrl + arrow keys
-vim.api.nvim_set_keymap('n', '<C-Up>', ':resize +2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Down>', ':resize -2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+-- Keymaps for starting and stopping the live server
+vim.keymap.set('n', '<leader>ls', ':StartLiveServer<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>lq', ':StopLiveServer<CR>', { noremap = true, silent = true })
