f26794cb4f4a5a3dc44be4585107e3629c25cd20
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-27 11:23:09 +0300

    feat: code runner

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@ require 'core.keymaps'
 require 'core.autocmds'
 require 'core.lazy'
 require 'core.statusline'
+require('core.runner').setup {}
 vim.lsp.enable { 'sqlls', 'html', 'ruff', 'ts_ls', 'pylsp', 'lua_ls', 'clangd', 'rust_analyzer' }
