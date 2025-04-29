476113c8bd853b9d1a3e0c7c1ec22ddf5ff2d78d
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-29 22:28:14 +0300

    feat: blink autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -90,3 +90,16 @@ vim.api.nvim_create_autocmd('ColorScheme', {
     vim.o.statusline = "%!v:lua.require'core.statusline'.statusline()"
   end,
 })
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuOpen',
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BlinkCmpMenuClose',
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
