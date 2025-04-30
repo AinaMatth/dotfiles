05cb858a61d90a19cab9d140ffb63c46efdd0010
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-30 13:26:40 +0300

    fix: styling

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,71 +1,73 @@
 return {
-  'saghen/blink.cmp',
-  event = { 'InsertEnter', 'CmdlineEnter' },
-  version = '1.*',
-  build = 'cargo build --release',
-  dependencies = {
-    -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
+  {
+    'saghen/blink.cmp',
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    version = '1.*',
+    build = 'cargo build --release',
+    dependencies = {
+      -- Snippet Engine
+      {
+        'L3MON4D3/LuaSnip',
+        version = '2.*',
+        build = (function()
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+        dependencies = {
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+        opts = {},
+      },
+      {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+          library = {
+            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+            { path = 'snacks.nvim', words = { 'Snacks' } },
+          },
         },
       },
-      opts = {},
+      { 'fang2hou/blink-copilot' },
     },
-    {
-      'folke/lazydev.nvim',
-      ft = 'lua',
-      opts = {
-        library = {
-          { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-          { path = 'snacks.nvim', words = { 'Snacks' } },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = { preset = 'default' },
+      appearance = {
+        nerd_font_variant = 'normal',
+      },
+      completion = {
+        ghost_text = {
+          enabled = true,
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 100,
         },
       },
-    },
-    { 'fang2hou/blink-copilot' },
-  },
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      nerd_font_variant = 'normal',
-    },
-    completion = {
-      ghost_text = {
-        enabled = true,
-      },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 100,
-      },
-    },
-    sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'copilot' },
-      providers = {
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
-        copilot = {
-          name = 'copilot',
-          module = 'blink-copilot',
-          score_offset = 100,
-          async = true,
+      sources = {
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'copilot' },
+        providers = {
+          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
         },
       },
+      snippets = { preset = 'luasnip' },
+      signature = { enabled = true },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
     },
-    snippets = { preset = 'luasnip' },
-    signature = { enabled = true },
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
   },
 }
