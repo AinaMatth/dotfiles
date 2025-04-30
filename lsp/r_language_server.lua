631a6be0ac435f715e789193e28d2f05f1395727
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-04-30 13:26:20 +0300

    feat: r_lsp

--- lsp/r_language_server.lua
+++ lsp/r_language_server.lua
@@ -1,12 +1,6 @@
 return {
   cmd = { 'R', '--no-echo', '-e', 'languageserver::run()' },
   filetypes = { 'r', 'rmd', 'quarto' },
-  settings = {
-    ['r_language_server'] = {
-      on_attach = function(client, _)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-      end,
-    },
-  },
+  root_markers = { 'air.toml', '.air.toml', '.git' },
+  settings = {},
 }
