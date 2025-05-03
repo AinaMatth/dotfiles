c21c1899a20fa76dd2a734f905ce93925e11c2cc
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-03 21:17:58 +0300

    chore: update keys

--- lua/plugins/productivity/todocomments.lua
+++ lua/plugins/productivity/todocomments.lua
@@ -9,6 +9,26 @@ return {
   },
   keys = {
     {
+      ']t',
+      function()
+        require('todo-comments').jump_next()
+      end,
+      desc = 'Next Todo Comment',
+    },
+    {
+      '[t',
+      function()
+        require('todo-comments').jump_prev()
+      end,
+      desc = 'Previous Todo Comment',
+    },
+    { '<leader>xt', '<cmd>Trouble todo toggle<cr>', desc = 'Todo (Trouble)' },
+    {
+      '<leader>xT',
+      '<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>',
+      desc = 'Todo/Fix/Fixme (Trouble)',
+    },
+    {
       '<leader>ft',
       function()
         Snacks.picker.todo_comments { keywords = { 'TODO', 'HACK', 'WARNING', 'BUG', 'NOTE', 'INFO', 'PERF', 'ERROR' } }
