46cfc2c66ef8ae7668893038f16d9dae8a8b285e
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-11 10:32:55 +0300

    style: icons

--- neovim/.config/nvim/lua/plugins/misc/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/misc/gitsigns.lua
@@ -1,19 +1,19 @@
 return {
   'lewis6991/gitsigns.nvim',
-  event = 'BufReadPre',
+  event = { 'BufReadPre', 'BufNewFile' },
   signs = {
     add = { text = '▎' },
     change = { text = '▎' },
-    delete = { text = '' },
-    topdelete = { text = '' },
+    delete = { text = '▎' },
+    topdelete = { text = '▎' },
     changedelete = { text = '▎' },
     untracked = { text = '▎' },
   },
   signs_staged = {
     add = { text = '▎' },
     change = { text = '▎' },
-    delete = { text = '' },
-    topdelete = { text = '' },
+    delete = { text = '▎' },
+    topdelete = { text = '▎' },
     changedelete = { text = '▎' },
   },
 }
