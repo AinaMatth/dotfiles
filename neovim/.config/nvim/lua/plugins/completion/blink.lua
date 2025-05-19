3011ed133de13a12333a2c473cb0814a0c5ebe8a
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-19 10:06:37 +0300

    refactor: trim config file

--- neovim/.config/nvim/lua/plugins/completion/blink.lua
+++ neovim/.config/nvim/lua/plugins/completion/blink.lua
@@ -1,60 +1,24 @@
 return {
   {
-    'saghen/blink.compat',
-    version = '*',
-    lazy = true,
-    opts = {},
-  },
-  {
     'saghen/blink.cmp',
-    event = { 'InsertEnter', 'CmdlineEnter' },
-    version = '1.*',
+    event = 'InsertEnter',
+    version = '*',
     build = 'cargo build --release',
     dependencies = {
-      -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          {
-            'rafamadriz/friendly-snippets',
-            config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
-            end,
-          },
-        },
-        opts = {},
-      },
-      {
-        'folke/lazydev.nvim',
-        ft = 'lua',
-        opts = {
-          library = {
-            { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-            { path = 'snacks.nvim', words = { 'Snacks' } },
-          },
-        },
-      },
+      'rafamadriz/friendly-snippets',
       { 'fang2hou/blink-copilot' },
     },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'enter',
+        ['<c-y>'] = { 'show_documentation', 'hide_documentation' },
+      },
       appearance = {
         nerd_font_variant = 'normal',
       },
       completion = {
-        menu = {
-          border = 'rounded',
-          scrollbar = false,
-        },
         ghost_text = {
           enabled = true,
           show_with_selection = true,
@@ -63,13 +27,13 @@ return {
         documentation = {
           auto_show = true,
           auto_show_delay_ms = 100,
+          treesitter_highlighting = true,
           window = { border = 'rounded' },
         },
       },
       sources = {
-        default = { 'lazydev', 'lsp', 'path', 'snippets', 'copilot' },
+        default = { 'lsp', 'path', 'snippets', 'copilot' },
         providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
           copilot = {
             name = 'copilot',
             module = 'blink-copilot',
@@ -78,10 +42,13 @@ return {
           },
         },
       },
-      snippets = { preset = 'luasnip' },
-      signature = { enabled = true, window = { border = 'rounded' } },
       fuzzy = { implementation = 'prefer_rust_with_warning' },
+      cmdline = {
+        enabled = false,
+      },
     },
-    opts_extend = { 'sources.default' },
+    opts_extend = {
+      'sources.default',
+    },
   },
 }
