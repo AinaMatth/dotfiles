40a4b743078c22de144b99cf645c40dc948273e6
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-19 10:05:44 +0300

    feat: quarto autocommands
    - Quarto Preview autocommand

--- neovim/.config/nvim/lua/core/autocmds.lua
+++ neovim/.config/nvim/lua/core/autocmds.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+    vim.hl.on_yank { higroup = 'IncSearch', timeout = 200 }
   end,
 })
 -- show cursor line only in active window
@@ -65,5 +65,36 @@ vim.api.nvim_create_autocmd('User', {
   pattern = 'BlinkCmpMenuClose',
   callback = function()
     vim.b.copilot_suggestion_hidden = false
+  end,
+})
+
+local function quarto_preview_in_split()
+  local filepath = vim.fn.expand '%:p' -- full path to current file
+  local filename = vim.fn.expand '%:t' -- just the file name
+  local filedir = vim.fn.expand '%:p:h' -- directory
+
+  if not filepath:match '%.qmd$' then
+    vim.notify('Not a .qmd file', vim.log.levels.WARN)
+    return
+  end
+
+  -- Open terminal split
+  vim.cmd 'belowright split | resize 15'
+
+  -- Run: cd to file's dir, then preview the file
+  local cmd = string.format('cd "%s" && quarto preview "%s"', filedir, filename)
+  vim.cmd('terminal ' .. cmd)
+
+  -- Focus into terminal
+  vim.cmd 'startinsert'
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'quarto',
+  callback = function()
+    vim.keymap.set('n', '<leader>qp', quarto_preview_in_split, {
+      buffer = true,
+      desc = 'Quarto Preview in Terminal',
+    })
   end,
 })
