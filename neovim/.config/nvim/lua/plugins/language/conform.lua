c23f948adafdfaef84a7bd4b1fd4fee3a25779de
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-19 10:07:18 +0300

    fix: injected R formatter

--- neovim/.config/nvim/lua/plugins/language/conform.lua
+++ neovim/.config/nvim/lua/plugins/language/conform.lua
@@ -3,31 +3,42 @@ return {
   dependencies = { 'mason.nvim' },
   cmd = { 'ConformInfo', 'ConformFormat', 'ConformFormat', 'ConformToggleFormatGlobal' },
   event = 'BufWritePre',
-  ---@module "conform"
-  ---@type conform.setupOpts
-  opts = {
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      -- sql = { 'sqruff', stop_after_first = true },
-      -- rust = { "rustfmt", lsp_format = "fallback" },
-      javascript = { 'prettierd', 'prettier', stop_after_first = true },
-      html = { 'prettierd', 'prettier', stop_after_first = true },
-      c = { 'clang-format' },
-      -- quarto = { 'injected' },
-      -- rmd = { 'injected' },
-      r = { 'air' },
-    },
-    default_format_opts = {
-      timeout_ms = 3000,
-      async = false,
-      quiet = false,
-      lsp_format = 'fallback',
-    },
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_format = 'fallback',
-    },
-  },
+  config = function()
+    require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- sql = { 'sqruff', stop_after_first = true },
+        -- rust = { "rustfmt", lsp_format = "fallback" },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        c = { 'clang-format' },
+        quarto = { 'injected' },
+        rmd = { 'injected' },
+        r = { 'air' },
+      },
+      default_format_opts = {
+        timeout_ms = 3000,
+        async = false,
+        quiet = false,
+        lsp_format = 'fallback',
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      },
+    }
+    require('conform').formatters.injected = {
+      options = {
+        ignore_errors = false,
+        lang_to_ext = {
+          markdown = 'md',
+          python = 'py',
+          r = 'r',
+        },
+        lang_to_formatters = {},
+      },
+    }
+  end,
   init = function()
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
