db408d3430997a96d223dfb0d61e9afbebc85158
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-19 10:08:33 +0300

    feat: catppuccin -> kanso

--- neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,52 +1,13 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'webhooked/kanso.nvim',
+  lazy = false,
   priority = 1000,
   config = function()
-    require('catppuccin').setup {
-      flavour = 'mocha',
-      transparent_background = true,
-      styles = {
-        comments = {},
-      },
-      compile_path = vim.fn.stdpath 'cache' .. '/catppuccin',
-      integrations = {
-        blink_cmp = true,
-        fzf = true,
-        copilot_vim = true,
-        notify = true,
-
-        gitsigns = true,
-        mason = true,
-        treesitter = true,
-        render_markdown = true,
-        mini = {
-          enabled = true,
-          indent_scope_color = '',
-        },
-        which_key = true,
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { 'italic' },
-            hints = { 'italic' },
-            warnings = { 'italic' },
-            information = { 'italic' },
-            ok = { 'italic' },
-          },
-          underlines = {
-            errors = { 'underline' },
-            hints = { 'underline' },
-            warnings = { 'underline' },
-            information = { 'underline' },
-            ok = { 'underline' },
-          },
-          inlay_hints = {
-            background = true,
-          },
-        },
-      },
+    require('kanso').setup {
+      compile = true,
+      commentStyle = { italic = false },
+      transparent = true,
     }
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'kanso'
   end,
 }
