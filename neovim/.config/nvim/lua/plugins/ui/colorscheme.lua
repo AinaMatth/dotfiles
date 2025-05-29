840f06d0488eb83a03e4dfc9e2b217760cb8bfe5
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-29 21:12:40 +0300

    style: highlights group

--- neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -3,7 +3,6 @@ return {
   config = function()
     require('kanagawa').setup {
       compile = true,
-      transparent = true,
       commentStyle = { italic = false },
       colors = {
         theme = {
@@ -16,6 +15,10 @@ return {
       },
       overrides = function(colors)
         local theme = colors.theme
+        local makeDiagnosticColor = function(color)
+          local c = require 'kanagawa.lib.color'
+          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+        end
         return {
           NormalFloat = { bg = 'none' },
           FloatBorder = { bg = 'none' },
@@ -27,6 +30,18 @@ return {
           PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
           PmenuSbar = { bg = theme.ui.bg_m1 },
           PmenuThumb = { bg = theme.ui.bg_p2 },
+          TelescopeTitle = { fg = '#DCD7BA', bg = '#FF5D62', bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewTitle = { fg = '#DCD7BA', bg = '#A3D4D5', bold = true },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
+          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
+          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
+          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
         }
       end,
     }
