c8c7075ace97bdffd7977268410c8768fc7036d3
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-29 21:37:16 +0300

    chore: update

--- neovim/.config/nvim/lua/plugins/language/miniai.lua
+++ neovim/.config/nvim/lua/plugins/language/miniai.lua
@@ -1,20 +1,34 @@
 return {
   'echasnovski/mini.ai',
-  version = false,
   event = 'VeryLazy',
-  config = function()
-    require('mini.ai').setup {
+  opts = function()
+    local ai = require 'mini.ai'
+    return {
       n_lines = 500,
       custom_textobjects = {
-        b = { { '%b()', '%b[]', '%b{}' }, '^.().*().$' },
-        q = { { [["[^"]-"]], [[`[^`]-`]], [[\'[^\']-\']] }, '^.().*().$' },
-        t = { { '<(%w-)[^>]->.-</%1>', '<(%w-)[^/>]-/>' }, '^.().*().$' },
-        l = { { '^.*().*$' } },
-        c = {
-          { '```.-```', '~~~.-~~~' },
-          '^([`~]{3}[^\n]*\n)().-()\n([`~]{3})$',
+        -- Treesitter-powered objects
+        o = ai.gen_spec.treesitter {
+          a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+          i = { '@block.inner', '@conditional.inner', '@loop.inner' },
         },
+        f = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+        c = ai.gen_spec.treesitter { a = '@class.outer', i = '@class.inner' },
+
+        -- Quarto/Rmd code chunks: select entire ```{r} ... ``` blocks
+        q = {
+          '```{%w+}.-```',
+          '^()```{%w+}().-()```()$',
+        },
+
+        -- Tag (HTML/XML/Markdown)
+        t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' },
+        -- Function calls (general)
+        u = ai.gen_spec.function_call(),
+        U = ai.gen_spec.function_call { name_pattern = '[%w_]+' },
       },
     }
+  end,
+  config = function(_, opts)
+    require('mini.ai').setup(opts)
   end,
 }
