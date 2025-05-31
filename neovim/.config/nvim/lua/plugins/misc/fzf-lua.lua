36971e38162750cbd6ace5f2a041b8dad94290ce
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-31 17:54:17 +0300

    feat: revert to fzf

---
+++ neovim/.config/nvim/lua/plugins/misc/fzf-lua.lua
@@ -0,0 +1,26 @@
+return {
+  'ibhagwan/fzf-lua',
+  cmd = 'FzfLua',
+  keys = {
+    --stylua:ignore
+    { '<leader>sf', '<cmd>FzfLua files<cr>', desc = 'Find Project Files' },
+    { '<leader>sc', "<cmd>FzfLua files cwd='./.dotfiles'<cr>", desc = 'Find Dot Files' },
+    { '<leader>sg', '<cmd>FzfLua live_grep_native<cr>', desc = 'Live Grep' },
+    { '<leader>sr', '<cmd>FzfLua resume<cr>', desc = 'Resume' },
+    { '<leader>sb', '<cmd>FzfLua blines<cr>', desc = 'Buffer lines' },
+    { '<leader><leader>', '<cmd>FzfLua buffers<cr>', desc = 'Buffers' },
+    { '<leader>/', '<cmd>FzfLua lgrep_curbuf<cr>', desc = 'Grep Buffer' },
+    { '<leader>sh', '<cmd>FzfLua help_tags<cr>', desc = 'Help Tags' },
+    { '<leader>sd', '<cmd>FzfLua lsp_document_symbols<cr>', desc = 'Document Symbols' },
+    { '<leader>sw', '<cmd>FzfLua lsp_workspace_symbols<cr>', desc = 'Workspace Symbols' },
+    { '<leader>sD', '<cmd>FzfLua diagnostics_documents<cr>', desc = 'Diagnostics' },
+  },
+  opts = {
+    { 'max-perf', 'hide' },
+    winopts = {
+      preview = {
+        border = 'border-top',
+      },
+    },
+  },
+}
