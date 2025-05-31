6f13a2e1bb3ff137d3623ba1fc6b96f637ec52fd
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-31 17:53:58 +0300

    style: catppuccin

--- neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,50 +1,16 @@
 return {
-  'rebelot/kanagawa.nvim',
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
   config = function()
-    require('kanagawa').setup {
-      compile = true,
-      commentStyle = { italic = false },
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = 'none',
-            },
-          },
-        },
+    require('catppuccin').setup {
+      styles = {
+        comments = {},
       },
-      overrides = function(colors)
-        local theme = colors.theme
-        local makeDiagnosticColor = function(color)
-          local c = require 'kanagawa.lib.color'
-          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
-        end
-        return {
-          NormalFloat = { bg = 'none' },
-          FloatBorder = { bg = 'none' },
-          FloatTitle = { bg = 'none' },
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-          LazyNormal = { bg = 'none', fg = theme.ui.fg_dim },
-          MasonNormal = { bg = 'none', fg = theme.ui.fg_dim },
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
-          PmenuSel = { fg = 'NONE', bg = theme.ui.bg_p2 },
-          PmenuSbar = { bg = theme.ui.bg_m1 },
-          PmenuThumb = { bg = theme.ui.bg_p2 },
-          TelescopeTitle = { fg = '#DCD7BA', bg = '#FF5D62', bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewTitle = { fg = '#DCD7BA', bg = '#A3D4D5', bold = true },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
-          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
-          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
-          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
-        }
-      end,
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
     }
-    vim.cmd 'colorscheme kanagawa'
+    vim.cmd 'colorscheme catppuccin'
   end,
 }
