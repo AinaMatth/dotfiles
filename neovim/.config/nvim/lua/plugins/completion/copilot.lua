5d75b59146ec5a626258002f1dce3c6a578f47ee
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-31 17:55:20 +0300

    fix: config

--- neovim/.config/nvim/lua/plugins/completion/copilot.lua
+++ neovim/.config/nvim/lua/plugins/completion/copilot.lua
@@ -1,13 +1,18 @@
 return {
   'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
+    panel = { enabled = false },
     suggestions = {
-      auto_trigger = false,
-      debounce = 75,
-      ignore_default_suggestions = false,
-      use_context = true,
+      hide_during_completion = false,
+      keymap = {
+        accept = '<C-.>',
+        accept_word = '<M-w>',
+        accept_line = '<M-l>',
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-/>',
+      },
     },
     filetypes = {
       markdown = true,
