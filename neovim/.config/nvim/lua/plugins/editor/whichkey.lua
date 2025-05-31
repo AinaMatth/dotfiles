7704a692eb40fb51ffef455fa77183ed1dd82e94
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-05-31 17:55:03 +0300

    style: search

--- neovim/.config/nvim/lua/plugins/editor/whichkey.lua
+++ neovim/.config/nvim/lua/plugins/editor/whichkey.lua
@@ -12,7 +12,7 @@ return {
         { '<leader>', group = 'Leader', icon = { icon = '󰓎 ', color = 'yellow' } },
         { '<leader>c', group = 'code' },
         { '<leader>x', group = 'diagnostics/quickfix', icon = { icon = '󱖫 ', color = 'green' } },
-        { '<leader>s', group = '[T]elescope', icon = { icon = ' ', color = 'blue' } },
+        { '<leader>s', group = '[F]zF', icon = { icon = ' ', color = 'white' } },
         { '<leader>g', group = 'Git [H]unk' },
         { 'gx', desc = 'Open with system app' },
       },
