4eb37660eb28d3c56ed80b461537224fd3798518
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-02 08:43:55 +0300

    chore: update treesitter

--- neovim/.config/nvim/lua/plugins/language/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/language/treesitter.lua
@@ -43,13 +43,8 @@ return {
     opts = {
       install_dirs = vim.fn.stdpath 'data' .. '/treesitter-parsers',
       highlight = {
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
+        enable = true,
+        additional_vim_regex_highlighting = false,
       },
     },
     config = function(_, opts)
