ab7edd7585595518b3bcf6314f0ecf8b866f9980
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-02 08:43:46 +0300

    refactor: mason.lua

---
+++ neovim/.config/nvim/lua/plugins/language/mason.lua
@@ -0,0 +1,13 @@
+return {
+  'mason-org/mason.nvim',
+  opts = {
+    ui = {
+      icons = {
+        package_pending = ' ',
+        package_installed = ' ',
+        package_uninstalled = ' ',
+      },
+    },
+    max_concurrent_installers = 10,
+  },
+}
