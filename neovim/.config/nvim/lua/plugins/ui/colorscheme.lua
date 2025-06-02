59513c0a4f289b27ab0f5720d738693ae089df3a
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-02 16:28:29 +0300

    style: set transparent background

--- neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/ui/colorscheme.lua
@@ -7,7 +7,39 @@ return {
       styles = {
         comments = {},
       },
+      transparent_background = true,
+      integrations = {
+        blink_cmp = true,
+        flash = true,
+        fzf = true,
+        gitsigns = true,
+        illuminate = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { 'undercurl' },
+            hints = { 'undercurl' },
+            warnings = { 'undercurl' },
+            information = { 'undercurl' },
+          },
+        },
+        noice = true,
+        notify = true,
+        render_markdown = true,
+        semantic_tokens = true,
+        treesitter = true,
+        which_key = true,
+      },
     }
+    custom_highlights = function(colors)
+      return {
+        NormalFloat = { bg = colors.base, fg = colors.text },
+      }
+    end
     vim.cmd 'colorscheme catppuccin'
   end,
 }
