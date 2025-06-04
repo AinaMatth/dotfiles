7dcb15b98346e43f06672077ef7b0402fd5709ca
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-04 21:47:39 +0300

    delete: copilot

--- neovim/.config/nvim/lua/plugins/completion/copilot.lua
+++ neovim/.config/nvim/lua/plugins/completion/copilot.lua
@@ -1,22 +1,29 @@
 return {
   'zbirenbaum/copilot.lua',
   event = 'InsertEnter',
+  cmd = 'Copilot',
+  enabled = false,
   opts = {
     panel = { enabled = false },
     suggestions = {
-      hide_during_completion = false,
+      auto_trigger = false,
       keymap = {
-        accept = '<C-.>',
-        accept_word = '<M-w>',
-        accept_line = '<M-l>',
         next = '<M-]>',
         prev = '<M-[>',
-        dismiss = '<C-/>',
       },
     },
     filetypes = {
       markdown = true,
       help = true,
+    },
+  },
+  keys = {
+    {
+      '<leader>co',
+      function()
+        require('copilot.suggestion').toggle_auto_trigger()
+      end,
+      { desc = 'Toggle Copilot' },
     },
   },
 }
