5746ae09ea39ca89248b921f9e13453be9085175
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-04 21:47:28 +0300

    delete: copilot

--- neovim/.config/nvim/lua/core/autocmds.lua
+++ neovim/.config/nvim/lua/core/autocmds.lua
@@ -104,17 +104,3 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
   command = [[%s/\s\+$//e]],
 })
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'BlinkCmpMenuOpen',
-  callback = function()
-    vim.b.copilot_suggestion_hidden = true
-  end,
-})
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'BlinkCmpMenuClose',
-  callback = function()
-    vim.b.copilot_suggestion_hidden = false
-  end,
-})
