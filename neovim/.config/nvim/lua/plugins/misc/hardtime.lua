80b9e6491fd965fbe5bdd152e674f3e6e0f35b2b
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-05 22:09:11 +0300

    fix: toggle

--- neovim/.config/nvim/lua/plugins/misc/hardtime.lua
+++ neovim/.config/nvim/lua/plugins/misc/hardtime.lua
@@ -4,6 +4,6 @@ return {
   dependencies = { 'MunifTanjim/nui.nvim' },
   opts = {},
   keys = {
-    { '<leader>h', '<cmd>Hardtime toggle<CR>', desc = 'Toggle Hardtime' },
+    { '<leader>th', '<cmd>Hardtime toggle<CR>', desc = 'Toggle Hardtime' },
   },
 }
