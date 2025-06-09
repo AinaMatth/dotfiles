620849d5bcd0ddea903b58a76d9719d45d75de62
AinaMatth <110987778+AinaMatth@users.noreply.github.com>
2025-06-09 22:24:14 +0300

    style: mini.statusline

--- neovim/.config/nvim/plugin/20_mini.lua
+++ neovim/.config/nvim/plugin/20_mini.lua
@@ -42,9 +42,11 @@ now(function()
   statusline.section_location = function()
     return '%2l:%-2v'
   end
+  vim.opt.laststatus = 3
+  vim.opt.cmdheight = 0
 end)
 now(function()
-  require('mini.notify').setup { content = { sort = custom_sort } }
+  require('mini.notify').setup {}
   vim.notify = MiniNotify.make_notify {
     ERROR = { duration = 10000 },
   }
@@ -117,7 +119,7 @@ later(function()
   end, { desc = 'Trim trailing space and last blank lines' })
 end)
 later(function()
-  require('mini.pairs').setup { modes = { insert = true, command = true, terminal = true } }
+  require('mini.pairs').setup()
 end)
 later(function()
   require('mini.surround').setup()
@@ -185,24 +187,6 @@ later(function()
       { mode = 'n', keys = '<leader>r', desc = '+r' },
       { mode = 'n', keys = '<leader>c', desc = '+code' },
     },
-    window = {
-      delay = 300,
-      config = function(bufnr)
-        local max_width = 0
-
-        for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)) do
-          max_width = math.max(max_width, vim.fn.strchars(line))
-        end
-
-        -- Keep some right padding.
-        max_width = max_width + 2
-
-        return {
-          -- Dynamic width capped at 70.
-          width = math.min(70, max_width),
-        }
-      end,
-    },
   }
 end)
 later(function()
@@ -222,6 +206,42 @@ later(function()
   end
   vim.opt.complete = { '.', 'w', 'k', 'b', 'u' }
   vim.opt.completeopt:append 'fuzzy'
+  -- define keycodes
+  local keys = {
+    cn = vim.keycode '<c-n>',
+    cp = vim.keycode '<c-p>',
+    ct = vim.keycode '<c-t>',
+    cd = vim.keycode '<c-d>',
+    cr = vim.keycode '<cr>',
+    cy = vim.keycode '<c-y>',
+  }
+
+  -- select by <tab>/<s-tab>
+  vim.keymap.set('i', '<tab>', function()
+    -- popup is visible -> next item
+    -- popup is NOT visible -> add indent
+    return vim.fn.pumvisible() == 1 and keys.cn or keys.ct
+  end, { expr = true, desc = 'Select next item if popup is visible' })
+  vim.keymap.set('i', '<s-tab>', function()
+    -- popup is visible -> previous item
+    -- popup is NOT visible -> remove indent
+    return vim.fn.pumvisible() == 1 and keys.cp or keys.cd
+  end, { expr = true, desc = 'Select previous item if popup is visible' })
+
+  -- complete by <cr>
+  vim.keymap.set('i', '<cr>', function()
+    if vim.fn.pumvisible() == 0 then
+      -- popup is NOT visible -> insert newline
+      return require('mini.pairs').cr()
+    end
+
+    local item_selected = vim.fn.complete_info()['selected'] ~= -1
+    if item_selected then
+      -- popup is visible and item is selected -> complete item
+      return keys.cy
+    end
+    return keys.cy .. keys.cr
+  end, { expr = true, desc = 'Complete current item if item is selected' })
   local gen_loader = require('mini.snippets').gen_loader
   require('mini.snippets').setup {
     snippets = {
@@ -276,4 +296,7 @@ later(function()
       scroll_line = '▶',
     },
   }
+  vim.keymap.set('n', '<leader>tf', MiniMap.toggle_focus, { desc = 'MiniMap.toggle_focus' })
+  vim.keymap.set('n', '<leader>ts', MiniMap.toggle_side, { desc = 'MiniMap.toggle_side' })
+  vim.keymap.set('n', '<leader>tt', MiniMap.toggle, { desc = 'MiniMap.toggle' })
 end)
